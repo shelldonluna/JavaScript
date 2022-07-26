6d2ea91eb4b5750acf1cbe1418a131325a5140f7
Mauiricio Luna <shelldon.luna@gmail.com>
2022-07-26 13:12:52 -0500

    subienndo a git control de flujo

--- app.js
+++ app.js
@@ -1,98 +1,46 @@
 
 console.log('Hola mundo')
 
-//tipos de datos en JS, la verdad es que este tema ya lo sé
-//undefined variable no definida
-//object cualquier combinación de tipos de datos
+//control de flujo
+/* const edad = 5
+if (edad > 5 &&edad <18) {
+	console.log ('El niño puede jugar')
 
+} else {
+	console.log ('el niño no puede jugar')
+}
+ */
 
-//Definición de variables
-//var
-//let
-//const
+//control de flujo while
+const edad =5
+if (edad > 5 &&edad <18) {
+	console.log ('El niño puede jugar')
 
-//var miPrimeraVariable = 'lala'
+}
+let x=true
+while (x) {
+	console.log(x)
+	x = false
 
-/* let miPrimeraVariable='mi primera variable!'
-console.log(miPrimeraVariable);
-//mutabilidad
-miPrimeraVariable = 'Esto ha cambiado'
-console.log(miPrimeraVariable);
+}
 
-let miBoolean =  true
-let miOtroBoolean = false
-let miNumero = 123
-let miString = 'el perro'
-let undefin
-let nulo = null
+//console.log('terminando el lo')
 
-console.log(miBoolean, miOtroBoolean, miNumero, miString, undefin,nulo);
-
-//objeto
-//agrupacion de datos que hacen sentido entre si 
-const miPrimerObjeto = {
-	unNumero: 12,
-	unString: 'Esta cadena de caracteres',
-	unaCondicion: true,
-};
-
-console.log(miPrimerObjeto.unString);
-
-const arrVacio =[]
-const arr=[1,2,'hola','mundo', miPrimerObjeto]
-
-console.log (arrVacio, arr);
-arrVacio.push (5)
-arrVacio.push (3)
-arrVacio.push (1)
-arrVacio.push ('Hola')
-
-console.log (arrVacio, arr);
-
-const suma = 1+2
-const resta = 1-2
-const multiplicar = 2*3
-const division = 9/3
-
-console.log (suma,resta, multiplicar, division); */
-
-const modulo = 10 % 3
-console.log (modulo);
-
-let num = 5
-
-
-num +=5
-num -=5
-num *=5
-num /=5
-console.log (num)
-
-// opeeradores de comparacion
-//igualdad estricta
-const resultado1 = 5 === 6
-//igual dad no estricta
-const resultado2 = 5 === '5'
-
-const resultado3 = 0<6 
-const resultado4 = 6<6 
-const resultado5 = 5<= 6
-const resultado6 = 5<=5
-console.log (resultado3, resultado4);
-
-// desigualdad estricta
-
-const resultado7 = 5 !== 6
-const resultado8 = 5 !== '5'
-
-// operadores logicos or || and  && y not !
-//voy a agregar un comentario
-const resultadoOr = false || true
-
-const resultadoAnd = false && false
-
-const resultadoNot = !false
-
-const resultado1 = 5
-
-console.log(resultadoNot)
+//control de flujo -switch
+switch (3){
+	case 1: {
+		console.log('yo soy el caso 1');
+		break;
+	}
+	case 2: {
+		console.log ('chanchito feliz');
+		break;
+	}
+	case 3:{
+		console.log ('chanchito triste');
+		break;
+	}
+	default:
+		console.log ('no hay chanchitos');
+		break;
+}
