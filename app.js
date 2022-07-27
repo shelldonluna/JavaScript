8f703a57d03ea55ee70f25f21fd7362aaabdd84a
Mauricio Luna <shelldon.luna@gmail.com>
2022-07-26 21:07:29 -0500

    me quede en introduccion a funciones

--- app.js
+++ app.js
@@ -12,35 +12,59 @@ if (edad > 5 &&edad <18) {
  */
 
 //control de flujo while
-const edad =5
-if (edad > 5 &&edad <18) {
-	console.log ('El niño puede jugar')
+// const edad =5
+// if (edad > 5 &&edad <18) {
+// 	console.log ('El niño puede jugar')
 
-}
-let x=true
-while (x) {
-	console.log(x)
-	x = false
+// }
+// let x=true
+// while (x) {
+// 	console.log(x)
+// 	x = false
 
-}
+// }
 
 //console.log('terminando el lo')
 
 //control de flujo -switch
-switch (3){
-	case 1: {
-		console.log('yo soy el caso 1');
-		break;
-	}
-	case 2: {
-		console.log ('chanchito feliz');
-		break;
-	}
-	case 3:{
-		console.log ('chanchito triste');
-		break;
-	}
-	default:
-		console.log ('no hay chanchitos');
-		break;
+// let y = 3;
+
+// switch (y){
+// 	case 1: {
+// 		console.log('yo soy el caso 1');
+// 		break;
+// 	}
+// 	case 2: {
+// 		console.log ('chanchito feliz');
+// 		break;
+// 	}
+// 	case 3:{
+// 		console.log ('chanchito triste');
+// 		break;
+// 	}
+// 	default:
+// 		console.log ('no hay chanchitos');
+// 		break;
+// }
+
+// ciclo for
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i)
+
+// }
+
+// Accediendo a los elementos de un arreglo 
+
+// const numeros = [1,2,'hola',4,5]
+
+// for (let i = 0; i < numeros.length; i++) {
+//  	console.log(i)
+// }
+
+// for (let i = 0; i < numeros.length; i++) {
+// 	console.log(numeros[i])
+// }
+
+function iterar () {
+	
 }
